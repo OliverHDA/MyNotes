72f052e2ad4d09e568cbdce8942529b07eec0f8e
OliverHDA <OliverHD@yandex.ru>
2021-01-20 21:28:10 +0300

    Home Work #2

---
+++ app/src/main/java/ru/oliverhd/mynotes/model/Note.kt
@@ -0,0 +1,3 @@
+package ru.oliverhd.mynotes.model
+
+class Note(val title: String, val note: String, val color: Int)
---
+++ app/src/main/java/ru/oliverhd/mynotes/ui/MainActivity.kt
@@ -0,0 +1,30 @@
+package ru.oliverhd.mynotes.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import ru.oliverhd.mynotes.databinding.ActivityMainBinding
+import ru.oliverhd.mynotes.viewmodel.MainViewModel
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var ui: ActivityMainBinding
+    lateinit var viewModel: MainViewModel
+    lateinit var adapter: MainAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        ui = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(ui.root)
+
+        setSupportActionBar(ui.toolbar)
+        viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
+        adapter = MainAdapter()
+        ui.mainRecycler.adapter = adapter
+
+        viewModel.viewState().observe(this, Observer<MainViewState> { t ->
+            t?.let { adapter.notes = it.notes }
+        })
+    }
+}
